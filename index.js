8ded5f78c6ab412b6ae129a0c4d4f921f7cef10d
Srijan Singh <120157897+SrijanSingh9@users.noreply.github.com>
2025-01-04 12:57:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 const app = express();
 const PORT = 3000;
-const apiKey = "AIzaSyARtwMOt8j822f7a6_M3yoMtoEZ6TGcNeQ";
+const apiKey = "Your API Key :>";
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
